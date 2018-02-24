cbf1b8681a3055b465d31f4d135a5fca17e0f8ec
Adam Dąbrowski <contact@adamdabrowski.com>
2018-02-24 13:45:21 +0100

    Draw a rectangle

--- Roguelike/MonoGame.fs
+++ Roguelike/MonoGame.fs
@@ -6,6 +6,7 @@ open Microsoft.Xna.Framework.Graphics
 type MonoGame () as game =
     inherit Game ()
 
+    let mutable texture = Unchecked.defaultof<Texture2D>
     let mutable graphics = Unchecked.defaultof<GraphicsDeviceManager>
     let mutable spriteBatch = Unchecked.defaultof<SpriteBatch>
     do 
@@ -18,6 +19,10 @@ type MonoGame () as game =
      
     override game.LoadContent () =
         spriteBatch <- new SpriteBatch (graphics.GraphicsDevice)
+
+        texture <- new Texture2D (graphics.GraphicsDevice, 32, 32)
+        texture.SetData <| Array.create (32*32) Color.Chocolate
+
         ()
      
     override game.Update (gameTime) =
@@ -26,5 +31,10 @@ type MonoGame () as game =
      
     override game.Draw (gameTime) =
         game.GraphicsDevice.Clear Color.CornflowerBlue
+
+        spriteBatch.Begin ()
+        spriteBatch.Draw (texture, new Vector2 (float32 10, float32 20), Color.White)
+        spriteBatch.End ()
+
         base.Draw gameTime
         ()
